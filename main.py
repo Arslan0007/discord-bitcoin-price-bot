05f3883d12dfd97f54d3f9f55e78b13c240533a8
arslan0007 <arsla.n7.cl@gmail.com>
2024-08-07 21:34:03 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import discord
+import os
+from dotenv import load_dotenv, dotenv_values
+from discord.ext import commands
+
+load_dotenv()
+
+intents = discord.Intents.all()
+intents.message_content = True
+# client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix="!", intents=intents)
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(TOKEN)
+
