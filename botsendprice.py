0d461934e49cb82b16a45df111e39e9d428133cf
arslan0007 <arsla.n7.cl@gmail.com>
2024-08-21 20:55:17 +0300

    Fetches btc currencies as parameter

--- botsendprice.py
+++ botsendprice.py
@@ -1,29 +1,53 @@
 import discord
 import os
 import requests
-from dotenv import load_dotenv, dotenv_values
+from dotenv import load_dotenv
 from discord.ext import commands
 import json
+import pandas as pd
+from sqlalchemy.orm.sync import update
 
 load_dotenv()
 
 intents = discord.Intents.all()
-data = commands.Bot(command_prefix="$", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 
-@data.event
-async def on_message(message):
-    if message.content.startswith("!usd"):
-        url = "https://api.coindesk.com/v1/bpi/currentprice.json"
-        response = requests.get(url)
-        response_json = response.json()
-        response_usd_rate = response_json["bpi"]["USD"]["rate"]
-        with open('pricelist.txt', 'a') as p:
-            p.write(response_usd_rate)
-        with open('pricelist.txt', 'r') as f:
-            price = f.read()
-        print(price)
-        await message.channel.send(price)
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}!')
 
-data.run(TOKEN)
+@bot.command(description="Returns all commands")
+async def commands(self):
+    help_command = [c.name for c in self.client.commands]
+    print(help_command)
+
+@bot.command(name="price")
+async def get_bitcoin_price(ctx, currency):
+    url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+    response = requests.get(url)
+    response_json = response.json()
+    price = response_json['bpi'][currency]['rate']
+    print(price)
+    if currency == "USD":
+        with open('pricelist_usd.txt', 'w') as p:
+            p.write(price)
+        with open('pricelist_usd.txt', 'r') as f:
+            price_currency = f.read()
+        await ctx.channel.send(price_currency)
+    elif currency == "EUR":
+        with open('pricelist_eur.txt', 'w') as p:
+            p.write(price)
+        with open('pricelist_eur.txt', 'r') as f:
+            price_currency = f.read()
+        await ctx.channel.send(price_currency)
+    elif currency == "GBP":
+        with open('pricelist_gbp.txt', 'w') as p:
+            p.write(price)
+        with open('pricelist_gbp.txt', 'r') as f:
+            price_currency = f.read()
+        await ctx.channel.send(price_currency)
+    else:
+        pass
+bot.run(TOKEN)
