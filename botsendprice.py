9b80fcd28dc3ad9bf22be5e682f8bac556df628b
arslan0007 <arsla.n7.cl@gmail.com>
2024-08-25 15:26:36 +0300

    Fetches btc currencies as parameter without writing to a file and calling rate with lowercase

--- botsendprice.py
+++ botsendprice.py
@@ -4,8 +4,8 @@ import requests
 from dotenv import load_dotenv
 from discord.ext import commands
 import json
-import pprint
 
+from soupsieve.util import lower
 
 load_dotenv()
 
@@ -25,17 +25,18 @@ async def commands(self):
 
 @bot.command(name="price")
 async def get_bitcoin_price(ctx, currency):
-    url = "https://api.coindesk.com/v1/bpi/currentprice.json"
-    response = requests.get(url)
-    response_json = response.json()
-    price = response_json['bpi'][currency]['rate']
-    print(price)
-    if currency == "USD":
-        await ctx.channel.send(price)
-    elif currency == "EUR":
-        await ctx.channel.send(price)
-    elif currency == "GBP":
-        await ctx.channel.send(price)
-    else:
-        pass
+    # define the source
+    # fetch data from source
+    # convert data to json because it is easy to fetch specific data as json
+    # convert to lowercase
+    # define a parameter to fetch USD/EUR/GBP
+    # fetch a rate word
+    url = "https://api.coindesk.com/v1/bpi/currentprice.json" # define the source
+    response = requests.get(url) # fetching data with request from defined source
+    response_json = response.json() # converting data to json
+    price_cur = response_json['bpi'] # fetching specific area to find related currency
+    price_cur_lower = json.loads(json.dumps(price_cur).lower()) # convert to lowercase
+    price_cur_rate = price_cur_lower[currency]['rate'] # fetching related rate
+    await ctx.channel.send(price_cur_rate) # send the result
+
 bot.run(TOKEN)
